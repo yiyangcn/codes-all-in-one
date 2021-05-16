faf97525a1aa37501e4674c8346318b96c8e945c
yiyangcn <yiyangcn@outlook.com>
2021-05-16 17:08:46 +0800

    add a new question

---
+++ src/algorithms/stack/MinStack.java
@@ -0,0 +1,34 @@
+package algorithms.stack;
+import java.util.ArrayList;
+
+class MinStack {
+
+    /** initialize your data structure here. */
+    // https://leetcode-cn.com/problems/min-stack-lcci/
+    // 存放最小值
+    ArrayList<Integer> minStack;
+    ArrayList<Integer> stack;
+    public MinStack() {
+        minStack = new ArrayList<Integer>();
+        stack = new ArrayList<Integer>();
+        minStack.add(Integer.MAX_VALUE);
+    }
+    
+    public void push(int x) {
+        stack.add(x);
+        minStack.add(Math.min(minStack.get(minStack.size() - 1), x ));
+    }
+    
+    public void pop() {
+        stack.remove(stack.size() - 1);
+        minStack.remove(minStack.size() - 1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+    
+    public int getMin() {
+        return minStack.get(minStack.size() - 1);
+    }
+}
--- src/algorithms/stack/README.md
+++ src/algorithms/stack/README.md
@@ -12,4 +12,15 @@ https://leetcode-cn.com/problems/three-in-one-lcci
 
 - 二维数组存储每个栈的元素，一维数组记录每个栈当前有多少元素
 
-  
+
+### MinStack
+
+https://leetcode-cn.com/problems/min-stack-lcci/
+
+==栈可用LinkedList实现==
+
+**方法1：辅助栈**
+
+思路：
+
+- 辅助栈先存放`Integer.MAX_VALUE`值，每次push都放入当前的最小值 （`Math.min()`方法）
