b72f1dfb204a47f1dccb5de63ca4493853f8e8e5
yiyangcn <yiyangcn@qq.com>
2023-04-14 07:58:01 +0800

    add LambdaUsageDemo

---
+++ src/main/java/lambda/LambdaUsageDemo.java
@@ -0,0 +1,38 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaUsageDemo {
+
+    public static void main(String[] args) {
+        List<String> strList = new ArrayList<>();
+        strList.add("1");
+        strList.add("2");
+        strList.add("3");
+        // 输出
+        strList.forEach( n -> System.out.println(n));
+        strList.forEach(System.out::println);
+
+
+
+        // 使用惰性求值获得链 再使用及早求值获得集合
+        List<Person> personList = new ArrayList<>();
+        Person person1 = new Person();
+        person1.setName("Alice");
+        person1.setGender("female");
+        Person person2 = new Person();
+        person2.setName("Tom");
+        person2.setGender("male");
+        personList.add(person1);
+        personList.add(person2);
+        // 获取name为”Tom“的集合
+        List<Person> filterPersonList = personList.stream().filter(f -> f.getName().equals("Tom")).collect(Collectors.toList());
+        filterPersonList.forEach(person -> System.out.println("name:" + person.getName()));
+        // 顺序执行
+        personList.stream().collect(Collectors.toList());
+
+
+    }
+}
---
+++ src/main/java/lambda/Person.java
@@ -0,0 +1,23 @@
+package lambda;
+
+public class Person {
+
+    private String name;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
